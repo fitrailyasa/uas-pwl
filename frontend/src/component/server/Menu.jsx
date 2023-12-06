f20d5f7863695f1c118ea0d60e7ff66804faeb8a
Fitra Ilyasa <nde.joker12@gmail.com>
2023-12-07 01:11:47 +0700

    update

--- frontend/src/component/server/Menu.jsx
+++ frontend/src/component/server/Menu.jsx
@@ -2,46 +2,31 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTachometerAlt, faHome, faUser, faBox, faTag, faHistory, faSignOut } from '@fortawesome/free-solid-svg-icons';
 
-function Menu() {
+function MenuItem({ path, icon, label }) {
+    const currentPath = window.location.pathname;
+    const isActive = currentPath === path;
+
+    return (
+        <li className={`nav-item ${isActive ? 'active' : ''}`}>
+            <a href={path} className={`nav-link text-white ${isActive ? 'active' : ''}`}>
+                <FontAwesomeIcon icon={icon} className="nav-icon" />
+                <p>{label}</p>
+            </a>
+        </li>
+    );
+}
+
+function Menu({ children, title, backlink }) {
     return (
         <nav className="mt-2">
             <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li className="nav-item">
-                    <a href="/admin/dashboard" className="nav-link text-white">
-                        <FontAwesomeIcon icon={faTachometerAlt} className="nav-icon" />
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a href="/" className="nav-link text-white">
-                        <FontAwesomeIcon icon={faHome} className="nav-icon" />
-                        <p>Beranda</p>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a href="/admin/user" className="nav-link text-white">
-                        <FontAwesomeIcon icon={faUser} className="nav-icon" />
-                        <p>Data User</p>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a href="/admin/produk" className="nav-link text-white">
-                        <FontAwesomeIcon icon={faBox} className="nav-icon" />
-                        <p>Data Produk</p>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a href="/admin/kategori" className="nav-link text-white">
-                        <FontAwesomeIcon icon={faTag} className="nav-icon" />
-                        <p>Data Kategori</p>
-                    </a>
-                </li>
-                <li className="nav-item">
-                    <a href="/admin/transaksi" className="nav-link text-white">
-                        <FontAwesomeIcon icon={faHistory} className="nav-icon" />
-                        <p>Riwayat Transaksi</p>
-                    </a>
-                </li>
+                <MenuItem path="/admin/dashboard" icon={faTachometerAlt} label="Dashboard" />
+                <MenuItem path="/" icon={faHome} label="Beranda" />
+                <MenuItem path="/admin/user" icon={faUser} label="Data User" />
+                <MenuItem path="/admin/produk" icon={faBox} label="Data Produk" />
+                <MenuItem path="/admin/kategori" icon={faTag} label="Data Kategori" />
+                <MenuItem path="/admin/transaksi" icon={faHistory} label="Riwayat Transaksi" />
+                {/* Add other menu items as needed */}
                 <li className="nav-item">
                     <form id="logout-form" action="#" method="POST" hidden>
                         <input type="hidden" name="_token" value="your_csrf_token_here" />
--- frontend/src/component/server/Table.jsx
+++ frontend/src/component/server/Table.jsx
@@ -1,13 +1,44 @@
-import React, { useEffect } from 'react';
-import $ from 'jquery';
+import React, { useState } from 'react';
 
 const Table = ({ children, title, col = [], createLink }) => {
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [searchQuery, setSearchQuery] = useState('');
 
-    useEffect(() => {
-        $(document).ready(function () {
-            $('#example1').DataTable();
-        });
-    }, []);
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+    const filteredItems = React.Children.toArray(children).filter((item) =>
+        Object.values(item.props).some((value) =>
+            value.toString().toLowerCase().includes(searchQuery.toLowerCase())
+        )
+    );
+
+    const currentItems = filteredItems.slice(indexOfFirstItem, indexOfLastItem);
+
+    const totalPages = Math.ceil(filteredItems.length / itemsPerPage);
+
+    const handlePaginationClick = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const handleItemsPerPageChange = (e) => {
+        setItemsPerPage(parseInt(e.target.value, 10));
+        setCurrentPage(1);
+    };
+
+    const handleSearchChange = (e) => {
+        setSearchQuery(e.target.value);
+        setCurrentPage(1);
+    };
+
+    const handlePrevClick = () => {
+        setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    };
+
+    const handleNextClick = () => {
+        setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    };
 
     return (
         <div className="col-lg-12 form-wrapper">
@@ -16,11 +47,29 @@ const Table = ({ children, title, col = [], createLink }) => {
                     <div className="d-flex justify-content-between mb-3">
                         <h5 className="card-title">Tabel Data {title}</h5>
                         {createLink && (
-                            <a href={createLink} className="btn btn-sm btn-primary float-end">Tambah {title}</a>
+                            <a href={createLink} className="btn btn-sm btn-primary">Tambah {title}</a>
                         )}
                     </div>
+                    <div className="d-flex mb-3 justify-content-between">
+                        <div className="d-flex justify-content-start">
+                            <label className="form-label mb-0 align-self-center me-2">Tampilkan: </label>
+                            <select className="form-select form-select-sm" value={itemsPerPage} onChange={handleItemsPerPageChange}>
+                                <option value={10}>10</option>
+                                <option value={25}>25</option>
+                                <option value={50}>50</option>
+                                <option value={100}>100</option>
+                            </select>
+                        </div>
+                        <input
+                            type="text"
+                            className="form-control form-control-sm justify-content-end col-2"
+                            placeholder="Cari..."
+                            value={searchQuery}
+                            onChange={handleSearchChange}
+                        />
+                    </div>
                     <div className="table-responsive">
-                        <table id="example1" className="table table-bordered table-striped">
+                        <table className="table table-bordered table-striped">
                             <thead>
                                 <tr>
                                     {col.map((col, index) => (
@@ -29,14 +78,44 @@ const Table = ({ children, title, col = [], createLink }) => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {children}
+                                {currentItems}
                             </tbody>
                         </table>
                     </div>
+                    <nav>
+                        <ul className="pagination">
+                            <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
+                                <button
+                                    className="page-link"
+                                    onClick={handlePrevClick}
+                                >
+                                    Sebelumnya
+                                </button>
+                            </li>
+                            {Array.from({ length: totalPages }).map((_, index) => (
+                                <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+                                    <button
+                                        className="page-link"
+                                        onClick={() => handlePaginationClick(index + 1)}
+                                    >
+                                        {index + 1}
+                                    </button>
+                                </li>
+                            ))}
+                            <li className={`page-item ${currentPage === totalPages ? 'disabled' : ''}`}>
+                                <button
+                                    className="page-link"
+                                    onClick={handleNextClick}
+                                >
+                                    Selanjutnya
+                                </button>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default Table;
