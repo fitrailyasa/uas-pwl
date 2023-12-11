78b3a275f36492040e8842305a6e20b2caf35a99
Fitra Ilyasa <nde.joker12@gmail.com>
2023-12-11 19:41:32 +0700

    update

--- frontend/src/components/server/Menu.jsx
+++ frontend/src/components/server/Menu.jsx
@@ -4,7 +4,7 @@ import { faTachometerAlt, faHome, faUser, faBox, faTag, faHistory, faSignOut } f
 
 function MenuItem({ path, icon, label }) {
     const currentPath = window.location.pathname;
-    const isActive = currentPath === path;
+    const isActive = currentPath.startsWith(path);
 
     return (
         <li className={`nav-item ${isActive ? 'active' : ''}`}>
@@ -21,7 +21,12 @@ function Menu({ children, title, backlink }) {
         <nav className="mt-2">
             <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <MenuItem path="/admin/dashboard" icon={faTachometerAlt} label="Dashboard" />
-                <MenuItem path="/" icon={faHome} label="Beranda" />
+                <li className="nav-item">
+                    <a href="/" className="nav-link text-white">
+                        <FontAwesomeIcon icon={faHome} className="nav-icon" />
+                        <p>Beranda</p>
+                    </a>
+                </li>
                 <MenuItem path="/admin/user" icon={faUser} label="Data User" />
                 <MenuItem path="/admin/produk" icon={faBox} label="Data Produk" />
                 <MenuItem path="/admin/kategori" icon={faTag} label="Data Kategori" />
